1ba9bcd7b04617ef740fefadbf7169598023c1a2
Maicol <maiclopeza@gmail.com>
2020-09-25 22:04:42 -0500

    temperatura

---
+++ tabla.py
@@ -0,0 +1,14 @@
+#TABLA
+'''Digitar un numero de una tabla de multiplicar y mpostrar la serie'''
+
+#INPUTS
+
+a=0
+tabla=0
+
+#PROCESS
+print ("ingrese su temperatura en Grados Centigrados: ")
+a= int(input())
+tabla=a*1
+tabla=a*2
+print ("La tabla es: ",tabla)
---
+++ temperatura.py
@@ -0,0 +1,12 @@
+#Temperatura
+''' Convertir grados centigrados a grados Kelvin'''
+
+#INPUTS
+centigrados=0
+kelvin=0
+
+#PROCESS
+print ("ingrese su temperatura en Grados Centigrados: ")
+centigrados= int(input())
+kelvin= centigrados+273.15
+print ("su temperatura en grados kelvin es: ",kelvin)
