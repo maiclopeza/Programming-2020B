71469ecb4ed7830b3b10917eb93c0a68eacd9511
Maicol <maiclopeza@gmail.com>
2020-09-25 22:11:00 -0500

    ejercicios

--- tabla.py
+++ tabla.py
@@ -3,12 +3,27 @@
 
 #INPUTS
 
-a=0
-tabla=0
-
 #PROCESS
-print ("ingrese su temperatura en Grados Centigrados: ")
+print ("ingrese un numero para generar la tabla: ")
 a= int(input())
-tabla=a*1
-tabla=a*2
-print ("La tabla es: ",tabla)
+n1=a*1
+n2=a*2
+n3=a*3
+n4=a*4
+n5=a*5
+n6=a*6
+n7=a*7
+n8=a*8
+n9=a*9
+n10=a*10
+
+print ("La tabla es ",n1)
+print ("La tabla es ",n2)
+print ("La tabla es ",n3)
+print ("La tabla es ",n4)
+print ("La tabla es ",n5)
+print ("La tabla es ",n6)
+print ("La tabla es ",n7)
+print ("La tabla es ",n8)
+print ("La tabla es ",n9)
+print ("La tabla es ",n10)
