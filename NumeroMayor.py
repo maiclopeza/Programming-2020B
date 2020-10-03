152e61671ec49e8a1a0487c231023222fb73e415
Maicol <maiclopeza@gmail.com>
2020-10-02 21:16:01 -0500

    Numero Mayor

--- NumeroMayor.py
+++ NumeroMayor.py
@@ -1,1 +1,14 @@
-print ("ejemplo")
+#Programa para comparar dos numeros y definir cual es el mayor#
+import os
+os.system 
+a=0
+b=0
+print ("Ingrese el primer numero")
+a=int(input())
+print ("Ingrese el segundo numero")
+b=int(input())
+if a>b
+    print("El numero mayor es:", a)
+else:
+    print ("El numero mayo es:", b)
+    
